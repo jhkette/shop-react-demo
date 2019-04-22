79909ca595c316acdbf222ed666ef15bd954a7b6
khenryjoe <joseph.ketterer@gmail.com>
2019-04-22 13:25:38 +0100

    change item page

--- src/Item.js
+++ src/Item.js
@@ -2,42 +2,26 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Item.css";
 
-// item takes an item prop and an add to cart prop too. 
-// returning a div that then get rendered as an li by parent. 
+// item takes an item prop and an add to cart prop too.
+// returning a div that then get rendered as an li by parent.
 
-const Item = ({
-  item,
-  onAddToCart
-}) =>
+const Item = ({ item, onAddToCart }) => (
   <div className="Item">
     <div className="Item-left">
       <div className="Item-image" />
-      <div className="Item-title">
-        {item.name}
-      </div>
-      <div className="Item-description">
-        {item.description}
-      </div>
+      <div className="Item-title">{item.name}</div>
+      <div className="Item-description">{item.description}</div>
     </div>
     <div className="Item-right">
-      <div className="Item-price">
-        ${item.price}
-      </div>
-      <button
-        className="Item-addToCart"
-        onClick={onAddToCart}
-      >
-        Add to Cart
-      </button>
+      <div className="Item-price">${item.price}</div>
+      {children}
     </div>
-  </div>;
+  </div>
+);
+
 Item.propTypes = {
   item: PropTypes.object.isRequired,
-  onAddToCart:
-    PropTypes.func.isRequired
+  children: PropTypes.node
 };
 
 export default Item;
-
- 
-  
